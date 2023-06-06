9562a3fccadfa224ceecbb8a96c29e3c3da35206
Samson Akanbi <samson.akanbi@outlook.com>
2023-06-06 22:55:02 +0100

    Created the ClientErrorException.

---
+++ src/main/java/com/samseen/bookify/core/controller/Home.java
@@ -0,0 +1,13 @@
+package com.samseen.bookify.core.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @GetMapping(value = {"/"})
+    public String home() {
+        return "redirect:/swagger-ui";
+    }
+}
---
+++ src/main/java/com/samseen/bookify/core/exception/ClientErrorException.java
@@ -0,0 +1,23 @@
+package com.samseen.bookify.core.exception;
+
+public class ClientErrorException extends RuntimeException {
+
+    public ClientErrorException() {
+    }
+
+    public ClientErrorException(String message) {
+        super(message);
+    }
+
+    public ClientErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ClientErrorException(Throwable cause) {
+        super(cause);
+    }
+
+    public ClientErrorException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
